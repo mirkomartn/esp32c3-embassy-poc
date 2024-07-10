f6e630bb07ecc9dcf7778b86a87c0fb0b2d0879a
Gasper Stukelj <gasperstukelj@proton.me>
2024-07-10 21:36:24 +0200

    Enable temperature sensing
    Enable the esp32-c3 built-in temperature sensor.
    
    The readings are printed raw (without computing
    the actual temperature). There's no HAL impl, so
    we're using board PAC crate (re-exported by HAL)
    to set up the sensor. We are "stealing" two
    singleton structs which is unsafe due to possible
    concurrent mutable access, but we're only setting
    register bits pertaining to the temperature
    sensor, so this is justified.
    
    Signed-off-by: Gasper Stukelj <gasperstukelj@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,42 @@ use esp_hal::{
 };
 use esp_println::println;
 
+struct Tsens {
+    tsens_reg: esp32c3::APB_SARADC,
+    sys_reg: esp32c3::SYSTEM,
+}
+
+impl Tsens {
+    fn new() -> Self {
+        let tsens = Self {
+            tsens_reg: unsafe { esp32c3::APB_SARADC::steal() },
+            sys_reg: unsafe { esp32c3::SYSTEM::steal() },
+        };
+        tsens.tsens_reg.tsens_ctrl().write(|w| w.pu().set_bit());
+        tsens
+            .sys_reg
+            .perip_clk_en1()
+            .write(|w| w.tsens_clk_en().set_bit());
+        // Select XTAL_CLK (default as per C idf source)
+        tsens
+            .tsens_reg
+            .tsens_ctrl2()
+            .write(|w| w.clk_sel().set_bit());
+        tsens
+    }
+}
+
+impl Drop for Tsens {
+    fn drop(&mut self) {
+        self.tsens_reg.tsens_ctrl().write(|w| w.pu().clear_bit());
+        self.sys_reg
+            .perip_clk_en1()
+            .write(|w| w.tsens_clk_en().clear_bit());
+    }
+}
+
 #[main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let peripherals = Peripherals::take();
     let system = SystemControl::new(peripherals.SYSTEM);
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
@@ -20,8 +54,21 @@ async fn main(_spawner: Spawner) {
     let timg0 = TimerGroup::new_async(peripherals.TIMG0, &clocks);
     esp_hal_embassy::init(&clocks, timg0);
 
+    spawner.spawn(tsens()).ok();
+
     loop {
         println!("Hello World");
         Timer::after(Duration::from_millis(5_000)).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn tsens() {
+    let tsens = Tsens::new();
+    Timer::after(Duration::from_micros(300)).await;
+
+    loop {
+        println!("Temp: {}", tsens.tsens_reg.tsens_ctrl().read().out().bits());
+        Timer::after(Duration::from_millis(2_000)).await;
     }
 }
