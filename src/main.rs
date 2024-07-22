1fc725681e507845487b2eac6ed0519304f0c968
Gasper Stukelj <gasperstukelj@proton.me>
2024-07-22 23:19:47 +0200

    App: implement bussines logic
    Implement the user-facing behavior. Every 5 seconds
    temperature is read and pushed to the public MQTT
    broker. Reading of temperature can be also triggered
    by pressing the BOOT button or by posting to a
    specific topic on the public MQTT broker.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use core::fmt::Write;
 use embassy_executor::Spawner;
+use embassy_futures::{
+    join::join,
+    select::{select, Either},
+};
 use embassy_net::{tcp::TcpSocket, Config, Ipv4Address, Stack, StackResources};
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::signal::Signal;
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::{
@@ -15,11 +22,23 @@ use esp_hal::{
     timer::{systimer::SystemTimer, timg::TimerGroup},
 };
 use esp_println::println;
+use heapless::String;
 
 mod boot_btn;
+mod mqtt;
 mod netstack;
 mod tsens;
 mod wifi;
+
+#[derive(Debug, Clone)]
+enum SigSource {
+    BootBtn,
+    Timer,
+    MQTT,
+    None,
+}
+
+static SIGNAL: Signal<CriticalSectionRawMutex, SigSource> = Signal::new();
 
 #[main]
 async fn main(spawner: Spawner) {
@@ -33,17 +52,24 @@ async fn main(spawner: Spawner) {
     let _ = esp_hal::gpio::Io::new(peripherals.GPIO, peripherals.IO_MUX);
 
     // setup boot button handler
-    boot_btn::start(&spawner, || println!("Hello button closure"));
+    boot_btn::start(&spawner, || SIGNAL.signal(SigSource::BootBtn));
 
-    let tsens = tsens::Tsens::new().await;
+    // setup timer that will raise signal every 5 seconds
+    spawner
+        .spawn(timer_trigger(5, || SIGNAL.signal(SigSource::Timer)))
+        .ok();
 
-    let mut wifi_link = wifi::WifiLink::new(
-        &spawner,
-        peripherals.SYSTIMER,
-        peripherals.RNG,
-        peripherals.RADIO_CLK,
-        &clocks,
-        peripherals.WIFI,
+    // poll two futures concurrently
+    let (tsens, mut wifi_link) = join(
+        tsens::Tsens::new(),
+        wifi::WifiLink::new(
+            &spawner,
+            peripherals.SYSTIMER,
+            peripherals.RNG,
+            peripherals.RADIO_CLK,
+            &clocks,
+            peripherals.WIFI,
+        ),
     )
     .await;
 
@@ -54,12 +80,63 @@ async fn main(spawner: Spawner) {
 
     let mut socket = netstack.get_tcp_socket(&mut rx_buffer, &mut tx_buffer);
 
-    netstack
+    if let Some(_) = netstack
         .connect_socket(&mut socket, "broker.hivemq.com", 1883)
-        .await;
+        .await
+    {
+        let mut rbuf = [0; 512];
+        let mut wbuf = [0; 512];
+        let rlen = rbuf.len();
+        let wlen = wbuf.len();
 
+        let mut mqtt = mqtt::MqttConnection::new(socket, &mut rbuf, rlen, &mut wbuf, wlen).await;
+
+        mqtt.subscribe("get-temp/1").await;
+
+        let mut msg: String<32> = String::new();
+
+        loop {
+            let mut src = SigSource::None;
+
+            SIGNAL.reset();
+
+            match select(SIGNAL.wait(), mqtt.recv_msg()).await {
+                Either::First(s) => {
+                    src = s;
+                }
+                Either::Second(Some(m)) => {
+                    if m.0 == "get-temp/1" && m.1 == b"it's me" {
+                        src = SigSource::MQTT;
+                    }
+                }
+                _ => {}
+            }
+
+            match src {
+                SigSource::BootBtn => {
+                    write!(msg, "BootBtn: {:.2}", tsens.get_temp()).ok();
+                }
+                SigSource::Timer => {
+                    write!(msg, "Timer: {:.2}", tsens.get_temp()).ok();
+                }
+                SigSource::MQTT => {
+                    write!(msg, "MQTT: {:.2}", tsens.get_temp()).ok();
+                }
+                _ => {
+                    continue;
+                }
+            }
+
+            mqtt.send_temp(&msg).await;
+            msg.clear();
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn timer_trigger(sec: u64, cb: impl Fn() + 'static) {
     loop {
-        println!("Temperature == {}", tsens.get_temp());
-        Timer::after(Duration::from_millis(5_000)).await;
+        Timer::after(Duration::from_secs(sec)).await;
+        cb();
     }
 }
